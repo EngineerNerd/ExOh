19a63e80d76b8f9af113c3e8922dd88e447cf2a8
Helen Spry <spry@w134-87-173-200.wireless.uvic.ca>
2018-06-23 18:07:04 -0700

    I made a method that prints the positions of the grid

--- Grid.py
+++ Grid.py
@@ -9,3 +9,15 @@ def sign(x):
 
 def duck():
 	print("ducks are cute")
+	
+
+def printGrid():
+	print("positions on grid:")
+	print('%d | %d | %d' % (0, 1, 2))
+	print("-   -   -")
+	print('%d | %d | %d' % (3, 4, 5))
+	print("-   -   -")
+	print('%d | %d | %d' % (6, 7, 8))
+
+
+printGrid()
