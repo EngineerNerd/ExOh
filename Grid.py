ae48ffea25c6d1069297906b97afedd3f08f911a
Helen Spry <spry@w134-87-173-200.wireless.uvic.ca>
2018-06-23 18:45:34 -0700

    There are methods to print the grid positions, an empty grid, and example of a filled in grid, and a method that may be able to print a gird throught a game

--- Grid.py
+++ Grid.py
@@ -11,7 +11,7 @@ def duck():
 	print("ducks are cute")
 	
 
-def printGrid():
+def printGridPositions():
 	print("positions on grid:")
 	print('%d | %d | %d' % (0, 1, 2))
 	print("-   -   -")
@@ -20,4 +20,41 @@ def printGrid():
 	print('%d | %d | %d' % (6, 7, 8))
 
 
-printGrid()
+def printGridEmpty():
+	print("empty grid:")
+	print("  |   |  ")
+	print("-   -   -")
+	print("  |   |  ")
+	print("-   -   -")
+	print("  |   |  ")
+
+
+def printGridExample():
+	print("an example game:")
+	print('%c | %c | %c' % ('X', 'O', 'O'))
+	print("-   -   -")
+	print('%c | %c | %c' % ('O', 'X', 'X'))
+	print("-   -   -")
+	print('%c | %c | %c' % ('X', 'O', 'O'))
+
+
+def printGridGame(zero, one, two, three, four, five, six, seven, eight):
+	print("the game so far:")
+	print( "{} | {} | {}".format(zero, one, two) )
+	print("-   -   -")
+	print( "{} | {} | {}".format(three, four, five) )
+	print("-   -   -")
+	print( "{} | {} | {}".format(six, seven, eight) )
+
+if __name__ == "__main__":
+	
+	print( "Hello {} {}".format("World", "Helen") )
+	
+	printGridPositions()
+	printGridEmpty()
+	printGridExample()
+	
+	printGridGame(" ", " ", "O", "O", " ", " ", "X", " ", "O")
+	printGridGame("X", "O", "O", "O", "X", "X", "X", "O", "O")
+	
+	pass
