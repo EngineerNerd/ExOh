7c4d1c053036fc436c17fd728a596a16081217f4
Sam Wilson <Sammyjwilson@me.com>
2018-06-23 18:10:36 -0700

    added game list'

--- Test.py
+++ Test.py
@@ -1,6 +1,15 @@
 from Grid import *
 
 
-number = 6
+number = -2
 
 print(sign(number)) 
+
+
+#game = {0: "None", 1: "None", 2: "None", 3: "None", 4: "None", 5: "None", 6: "None", 7: "None", 8: "None"}
+
+game = [[0,1,2],
+	   [3,4,5],
+       [6,7,8]]
+
+duck()
